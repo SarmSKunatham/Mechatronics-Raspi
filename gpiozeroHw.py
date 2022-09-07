d2c18741965410dd8c49a69fa16ddf4740d7caf4
Sarm Kunatham <sarmkunatham@Sarms-MacBook-Pro.local>
2022-09-07 16:19:25 +0700

    hw4

--- gpiozeroHw.py
+++ gpiozeroHw.py
@@ -1,17 +1,26 @@
-from gpiozero import Button, LED
-from signal import pause
+from gpiozero import Button, PWMLED
 from time import sleep
 
 led = LED(25)
 button = Button(24)
+value = 0
 while True:
     if button.is_pressed:
         print("pressed")
-        led.on()
+        if value >= 0.95:
+		value = 0
+		print(value)
+	else:
+		value = value + 0.05
+		print(value)
+		sleep(0.1)
+	led.value = value
         
     else:
         print("not")
-        led.off()
+        led.value = 0
+	value = 0
+	print(value)
  
 
 
